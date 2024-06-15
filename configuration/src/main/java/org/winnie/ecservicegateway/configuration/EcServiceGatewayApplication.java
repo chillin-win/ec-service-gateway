da9a944f28d2dd899f4c7bfb66c6890048b60d9e
cuongluongthien <cuongluong1313@gmail.com>
2024-06-15 21:31:46 +0700

    first commit

---
+++ configuration/src/main/java/org/winnie/ecservicegateway/configuration/EcServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.winnie.ecservicegateway.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcServiceGatewayApplication.class, args);
+    }
+
+}
